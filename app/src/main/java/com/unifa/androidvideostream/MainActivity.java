9dfa3d002c1046d944bb82bbb85a5d89b9e0bb43
Adnan <muh.adnansuryaazis@gmail.com>
2021-12-10 11:59:04 +0800

    successfully convert webview to image

--- app/src/main/java/com/unifa/androidvideostream/MainActivity.java
+++ app/src/main/java/com/unifa/androidvideostream/MainActivity.java
@@ -3,7 +3,10 @@ package com.unifa.androidvideostream;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +15,8 @@ import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -20,14 +25,18 @@ public class MainActivity extends AppCompatActivity {
     ProgressDialog pDialog;
     VideoView videoview;
 //    String vid_url ="https://www.youtube.com/watch?v=QnOcXQL2wDA&t=18s";
-    String vid_url ="192.168.1.4";
+    String vid_url ="192.168.1.5";
+
+    ImageView imView;
+    Button tesBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        imView = findViewById(R.id.imageView);
+        tesBtn = findViewById(R.id.button);
         WebView webView = findViewById(R.id.webView1);
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -42,9 +51,55 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
+        webView.setDrawingCacheEnabled(true);
         // Baris di bawah untuk menambahkan scrollbar di dalam WebView-nya
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+//        webView.setWebViewClient(new WebViewClient(){
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//
+//                Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+//                imView.setImageBitmap(bitmap);
+//            }
+//        });
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(vid_url);
+
+        tesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = viewToImage(MainActivity.this, webView);
+                imView.setImageBitmap(bitmap);
+
+            }
+        });
+    }
+
+    public static Bitmap viewToImage(Context context,
+                                     WebView viewToBeConverted) {
+        int extraSpace = 2000; //because getContentHeight doesn't always return the full screen height.
+        int height = viewToBeConverted.getContentHeight() + extraSpace;
+
+        Bitmap viewBitmap = Bitmap.createBitmap(
+                viewToBeConverted.getWidth(), height,
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(viewBitmap);
+        viewToBeConverted.draw(canvas);//w ww . ja  va 2 s. c  o m
+
+        //If the view is scrolled, cut off the top part that is off the screen.
+        try {
+            int scrollY = viewToBeConverted.getScrollY();
+
+            if (scrollY > 0) {
+                viewBitmap = Bitmap.createBitmap(viewBitmap, 0, scrollY,
+                        viewToBeConverted.getWidth(), height - scrollY);
+            }
+        } catch (Exception ex) {
+            Log.e("Bitmap","Could not remove top part of the webview image. ex: "
+                    + ex);
+        }
+
+        return viewBitmap;
     }
 }
